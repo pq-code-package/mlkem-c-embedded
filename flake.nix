02b90b61e78cd44d50d41369b0b5e469d0c26591
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-03-20 11:51:17 +0800

    add nix flake for dependencies
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "mlkem-c-embedded";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, system, inputs', ... }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+
+          devShells.default = with pkgs; mkShellNoCC {
+            packages = [
+              direnv
+              nix-direnv
+
+              gcc-arm-embedded-13
+              openocd
+              python312Packages.pyserial
+              astyle
+            ];
+
+            shellHook = ''
+              export PATH=$PWD/bin:$PATH
+            '';
+          };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
+
