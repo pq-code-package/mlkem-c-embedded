68db3d69a591d3e1299571b7ffb911b0c98f2f94
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-03-21 16:52:24 +0800

    add ci devshell
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -17,15 +17,20 @@
       imports = [ ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { pkgs, system, inputs', ... }:
+        let
+          core = with pkgs; [
+            # formatter & linters
+            astyle # 3.4.10
+
+            # build dependencies
+            gcc-arm-embedded-13 # arm-gnu-toolchain-13.2.rel1
+            openocd # 0.12.0
+            python311Packages.pyserial # 3.5
+          ];
+        in
         {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
-          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-
           devShells.default = with pkgs; mkShellNoCC {
-            packages = [
+            packages = core ++ [
               direnv
               nix-direnv
 
@@ -33,18 +38,17 @@
               nixpkgs-fmt
               shfmt
               codespell
-              astyle # 3.4.10
-
-              # build dependencies
-              gcc-arm-embedded-13 # arm-gnu-toolchain-13.2.rel1
-              openocd # 0.12.0
-              python311Packages.pyserial # 3.5
             ];
 
             shellHook = ''
               export PATH=$PWD/dev-support/bin:$PATH
             '';
           };
+
+          devShells.ci = with pkgs; mkShellNoCC {
+            packages = core;
+          };
+
         };
       flake = {
         # The usual flake attributes can be defined here, including system-
