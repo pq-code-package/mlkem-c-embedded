0deb3188590b5b9f78bbb42a61825d3da02814ed
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-04-27 14:39:38 +0800

    refactor with libopencm3 module gcc
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- mk/stm32f4discovery.mk
+++ mk/stm32f4discovery.mk
@@ -1,5 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 DEVICE=stm32f407vg
 OPENCM3_TARGET=lib/stm32/f4
+OPENCM3_DIR ?=
+override LDSCRIPT := obj/generated.$(DEVICE).ld
+override DEVICES_DATA := hal/devices.data
 
+include $(OPENCM3_DIR)/mk/genlink-config.mk
+include $(OPENCM3_DIR)/mk/gcc-config.mk
+include $(OPENCM3_DIR)/mk/genlink-rules.mk
+include $(OPENCM3_DIR)/mk/gcc-rules.mk
 include mk/opencm3.mk
