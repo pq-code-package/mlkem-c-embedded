1e7bf8833e58d9041383ae4e66ba455968fb43af
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-05-10 14:32:32 +0800

    move opencm3 configs inside mk/opencm3.mk
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- mk/opencm3.mk
+++ mk/opencm3.mk
@@ -1,4 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
+OPENCM3_DIR ?=
+override LDSCRIPT := obj/generated.$(DEVICE).ld
+
+include $(OPENCM3_DIR)/mk/genlink-config.mk
+include $(OPENCM3_DIR)/mk/genlink-rules.mk
+include $(OPENCM3_DIR)/mk/gcc-rules.mk
+
 LIBHAL_SRC += hal/hal-opencm3.c
 
 obj/libhal.a: $(call objs,$(LIBHAL_SRC))
