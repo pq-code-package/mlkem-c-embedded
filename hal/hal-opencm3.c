8dc24085f09e954ecd61aaa6001a83722639b8a2
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-05-14 18:25:35 +0800

    remove boards that are not supported
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- hal/hal-opencm3.c
+++ hal/hal-opencm3.c
@@ -38,49 +38,6 @@ const struct rcc_clock_scale benchmarkclock = {
     .apb2_frequency = 24000000,
 };
 
-#elif defined(STM32L476RG)
-#include <libopencm3/stm32/rng.h>
-
-#define SERIAL_GPIO GPIOA
-#define SERIAL_USART USART2
-#define SERIAL_PINS (GPIO2 | GPIO3)
-#define STM32
-#define NUCLEO_BOARD
-#elif defined(STM32F303RCT7)
-
-#define SERIAL_GPIO GPIOA
-#define SERIAL_USART USART1
-#define SERIAL_PINS (GPIO9 | GPIO10)
-#define STM32
-#define CW_BOARD
-#elif defined(STM32F415RGT6)
-#include <libopencm3/stm32/rng.h>
-
-#define SERIAL_GPIO GPIOA
-#define SERIAL_USART USART1
-#define SERIAL_PINS (GPIO9 | GPIO10)
-#define STM32
-#define CW_BOARD
-#elif defined(STM32L4R5ZI)
-#include <libopencm3/stm32/rng.h>
-#include <libopencm3/stm32/pwr.h>
-
-#define SERIAL_GPIO GPIOG
-#define SERIAL_USART LPUART1
-#define SERIAL_PINS (GPIO8 | GPIO7)
-#define NUCLEO_L4R5_BOARD
-
-/* Patched function for newer PLL not yet supported by opencm3 */
-void _rcc_set_main_pll(uint32_t source, uint32_t pllm, uint32_t plln, uint32_t pllp,
-                       uint32_t pllq, uint32_t pllr) {
-    RCC_PLLCFGR = (RCC_PLLCFGR_PLLM(pllm) << RCC_PLLCFGR_PLLM_SHIFT) |
-                  (plln << RCC_PLLCFGR_PLLN_SHIFT) |
-                  ((pllp & 0x1Fu) << 27u) | /* NEWER PLLP */
-                  (source << RCC_PLLCFGR_PLLSRC_SHIFT) |
-                  (pllq << RCC_PLLCFGR_PLLQ_SHIFT) |
-                  (pllr << RCC_PLLCFGR_PLLR_SHIFT) | RCC_PLLCFGR_PLLREN;
-}
-
 #elif defined(STM32F767ZI)
 #include <libopencm3/stm32/rng.h>
 #include <libopencm3/stm32/f7/rcc.h>
@@ -118,163 +75,8 @@ static void clock_setup(enum clock_mode clock) {
     }
 
     rcc_periph_clock_enable(RCC_RNG);
-    rng_enable();
 
     flash_prefetch_enable();
-    #elif defined(CW_BOARD)
-    (void) clock;
-    /* Some STM32 Platform */
-    rcc_periph_clock_enable(RCC_GPIOH);
-    rcc_osc_off(RCC_HSE);
-    rcc_osc_bypass_enable(RCC_HSE);
-    rcc_osc_on(RCC_HSE);
-    rcc_wait_for_osc_ready(RCC_HSE);
-
-    rcc_ahb_frequency = 7372800;
-    rcc_apb1_frequency = 7372800;
-    rcc_apb2_frequency = 7372800;
-    _clock_freq = 7372800;
-    rcc_set_hpre(RCC_CFGR_HPRE_DIV_NONE);
-    # if defined(STM32F3)
-    rcc_set_ppre1(RCC_CFGR_PPRE1_DIV_NONE);
-    rcc_set_ppre2(RCC_CFGR_PPRE2_DIV_NONE);
-    # elif defined(STM32F4)
-    rcc_set_ppre1(RCC_CFGR_PPRE_DIV_NONE);
-    rcc_set_ppre2(RCC_CFGR_PPRE_DIV_NONE);
-    #   if defined(STM32F415RGT6)
-    flash_set_ws(FLASH_ACR_LATENCY_0WS);
-    rcc_set_sysclk_source(RCC_CFGR_SW_HSE);
-    rcc_wait_for_sysclk_status(RCC_HSE);
-
-    /* HSI and PLL needed for RNG
-    that's why we can't use rcc_clock_setup_pll()*/
-    rcc_osc_on(RCC_HSI);
-    rcc_wait_for_osc_ready(RCC_HSI);
-    rcc_osc_off(RCC_PLL);
-    rcc_set_main_pll_hse(12, 196, 4, 7, 0);
-
-    rcc_osc_on(RCC_PLL);
-    rcc_wait_for_osc_ready(RCC_PLL);
-
-    rcc_periph_clock_enable(RCC_RNG);
-    rng_enable();
-    #   endif
-    # endif
-    rcc_set_sysclk_source(RCC_CFGR_SW_HSE);
-    rcc_wait_for_sysclk_status(RCC_HSE);
-    #elif defined(NUCLEO_BOARD)
-    /* NUCLEO-L476RG Board */
-    switch (clock) {
-    case CLOCK_BENCHMARK:
-        /* Benchmark straight from the HSI16 without prescaling */
-        rcc_osc_on(RCC_HSI16);
-        rcc_wait_for_osc_ready(RCC_HSI16);
-        rcc_ahb_frequency = 16000000;
-        rcc_apb1_frequency = 16000000;
-        rcc_apb2_frequency = 16000000;
-        _clock_freq = 16000000;
-        rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
-        rcc_set_ppre1(RCC_CFGR_PPRE_NODIV);
-        rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);
-        flash_dcache_enable();
-        flash_icache_enable();
-        flash_set_ws(FLASH_ACR_LATENCY_0WS);
-        flash_prefetch_enable();
-        rcc_set_sysclk_source(RCC_CFGR_SW_HSI16);
-        rcc_wait_for_sysclk_status(RCC_HSI16);
-        break;
-    case CLOCK_FAST:
-    default:
-        rcc_osc_on(RCC_HSI16);
-        rcc_wait_for_osc_ready(RCC_HSI16);
-        rcc_ahb_frequency = 80000000;
-        rcc_apb1_frequency = 80000000;
-        rcc_apb2_frequency = 80000000;
-        _clock_freq = 80000000;
-        rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
-        rcc_set_ppre1(RCC_CFGR_PPRE_NODIV);
-        rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);
-        rcc_osc_off(RCC_PLL);
-        while (rcc_is_osc_ready(RCC_PLL));
-        /* Configure the PLL oscillator (use CUBEMX tool -> scale HSI16 to 80MHz). */
-        rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 1, 10, RCC_PLLCFGR_PLLP_DIV7, RCC_PLLCFGR_PLLQ_DIV2, RCC_PLLCFGR_PLLR_DIV2);
-        /* Enable PLL oscillator and wait for it to stabilize. */
-        rcc_osc_on(RCC_PLL);
-        rcc_wait_for_osc_ready(RCC_PLL);
-        flash_dcache_enable();
-        flash_icache_enable();
-        flash_set_ws(FLASH_ACR_LATENCY_4WS);
-        flash_prefetch_enable();
-        rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
-        rcc_wait_for_sysclk_status(RCC_PLL);
-        break;
-    }
-
-    rcc_periph_clock_enable(RCC_RNG);
-    rng_enable();
-    #elif defined(NUCLEO_L4R5_BOARD)
-    rcc_periph_clock_enable(RCC_PWR);
-    rcc_periph_clock_enable(RCC_SYSCFG);
-    pwr_set_vos_scale(PWR_SCALE1);
-    /* The L4R5ZI chip also needs the R1MODE bit in PWR_CR5 register set, but
-       OpenCM3 doesn't support this yet. But luckily the default value for the bit
-       is 1. */
-    switch (clock) {
-    case CLOCK_BENCHMARK:
-        /* Benchmark straight from the HSI16 without prescaling */
-        rcc_osc_on(RCC_HSI16);
-        rcc_wait_for_osc_ready(RCC_HSI16);
-        rcc_ahb_frequency = 20000000;
-        rcc_apb1_frequency = 20000000;
-        rcc_apb2_frequency = 20000000;
-        _clock_freq = 20000000;
-        rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
-        rcc_set_ppre1(RCC_CFGR_PPRE_NODIV);
-        rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);
-        rcc_osc_off(RCC_PLL);
-        while (rcc_is_osc_ready(RCC_PLL));
-        /* Configure the PLL oscillator (use CUBEMX tool -> scale HSI16 to 20MHz). */
-        _rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 1, 10, 2, RCC_PLLCFGR_PLLQ_DIV2, RCC_PLLCFGR_PLLR_DIV8);
-        /* Enable PLL oscillator and wait for it to stabilize. */
-        rcc_osc_on(RCC_PLL);
-        flash_dcache_enable();
-        flash_icache_enable();
-        flash_set_ws(FLASH_ACR_LATENCY_0WS);
-        flash_prefetch_enable();
-        rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
-        rcc_wait_for_sysclk_status(RCC_PLL);
-        break;
-    case CLOCK_FAST:
-    default:
-        rcc_osc_on(RCC_HSI16);
-        rcc_wait_for_osc_ready(RCC_HSI16);
-        rcc_ahb_frequency = 120000000;
-        rcc_apb1_frequency = 120000000;
-        rcc_apb2_frequency = 120000000;
-        _clock_freq = 120000000;
-        rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
-        rcc_set_ppre1(RCC_CFGR_PPRE_NODIV);
-        rcc_set_ppre2(RCC_CFGR_PPRE_NODIV);
-        rcc_osc_off(RCC_PLL);
-        while (rcc_is_osc_ready(RCC_PLL));
-        /* Configure the PLL oscillator (use CUBEMX tool -> scale HSI16 to 120MHz). */
-        _rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 1, 15, 2, RCC_PLLCFGR_PLLQ_DIV2, RCC_PLLCFGR_PLLR_DIV2);
-        /* Enable PLL oscillator and wait for it to stabilize. */
-        rcc_osc_on(RCC_PLL);
-        rcc_wait_for_osc_ready(RCC_PLL);
-        flash_dcache_enable();
-        flash_icache_enable();
-        flash_set_ws(0x05);
-        flash_prefetch_enable();
-        rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
-        rcc_wait_for_sysclk_status(RCC_PLL);
-        break;
-    }
-    rcc_osc_on(RCC_HSI48); /* HSI48 must always be on for RNG */
-    rcc_wait_for_osc_ready(RCC_HSI48);
-    rcc_periph_clock_enable(RCC_RNG);
-    rcc_set_clock48_source(RCC_CCIPR_CLK48SEL_HSI48);
-    rng_enable();
     #elif defined(STM32F7)
     switch (clock) {
     case CLOCK_BENCHMARK:
@@ -287,7 +89,6 @@ static void clock_setup(enum clock_mode clock) {
     }
 
     rcc_periph_clock_enable(RCC_RNG);
-    rng_enable();
     flash_art_enable();
     flash_prefetch_enable();
     #else
@@ -296,40 +97,20 @@ static void clock_setup(enum clock_mode clock) {
 }
 
 void usart_setup() {
-    #if defined(DISCOVERY_BOARD)
+    #if defined(STM32F7)
+    rcc_periph_clock_enable(RCC_GPIOD);
+    rcc_periph_clock_enable(RCC_USART3);
+    #elif defined(DISCOVERY_BOARD)
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_USART2);
-    #elif defined(CW_BOARD)
-    rcc_periph_clock_enable(RCC_GPIOA);
-    rcc_periph_clock_enable(RCC_USART1);
     #elif defined(NUCLEO_BOARD)
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_USART2);
-    #elif defined(NUCLEO_L4R5_BOARD)
-    rcc_periph_clock_enable(RCC_GPIOG);
-    rcc_periph_clock_enable(RCC_LPUART1);
-
-    PWR_CR2 |= PWR_CR2_IOSV;
-    gpio_set_output_options(SERIAL_GPIO, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, SERIAL_PINS);
-    gpio_set_af(SERIAL_GPIO, GPIO_AF8, SERIAL_PINS);
-    gpio_mode_setup(SERIAL_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE, SERIAL_PINS);
-    usart_set_baudrate(SERIAL_USART, SERIAL_BAUD);
-    usart_set_databits(SERIAL_USART, 8);
-    usart_set_stopbits(SERIAL_USART, USART_STOPBITS_1);
-    usart_set_mode(SERIAL_USART, USART_MODE_TX_RX);
-    usart_set_parity(SERIAL_USART, USART_PARITY_NONE);
-    usart_set_flow_control(SERIAL_USART, USART_FLOWCONTROL_NONE);
-    usart_disable_rx_interrupt(SERIAL_USART);
-    usart_disable_tx_interrupt(SERIAL_USART);
-    usart_enable(SERIAL_USART);
-    #elif defined(STM32F7)
-    rcc_periph_clock_enable(RCC_GPIOD);
-    rcc_periph_clock_enable(RCC_USART3);
     #else
 #error Unsupported platform
     #endif
 
-    #if defined(DISCOVERY_BOARD) || defined(NUCLEO_BOARD) || defined(CW_BOARD) || defined(STM32F7)
+    #if defined(DISCOVERY_BOARD) || defined(NUCLEO_BOARD) || defined(STM32F7)
     gpio_set_output_options(SERIAL_GPIO, GPIO_OTYPE_OD, GPIO_OSPEED_100MHZ, SERIAL_PINS);
     gpio_set_af(SERIAL_GPIO, GPIO_AF7, SERIAL_PINS);
     gpio_mode_setup(SERIAL_GPIO, GPIO_MODE_AF, GPIO_PUPD_PULLUP, SERIAL_PINS);
@@ -357,6 +138,7 @@ void hal_setup(const enum clock_mode clock) {
     clock_setup(clock);
     usart_setup();
     systick_setup();
+    rng_enable();
 
     // wait for the first systick overflow
     // improves reliability of the benchmarking scripts since it makes it much
