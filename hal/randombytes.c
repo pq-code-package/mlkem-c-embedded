f5824faf6a8f038f1d6ddf6cd5995bc726bfefeb
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-08-16 14:38:21 +0800

    fix typo
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- hal/randombytes.c
+++ hal/randombytes.c
@@ -5,7 +5,7 @@
 #include <esp_random.h>
 int randombytes(uint8_t *obuf, size_t len) {
     // NOTE:
-    // After enableing the internal entropy source (SAR ADC), random numbers are then generated based on the thermal noise in the system and the asynchronous clock mismatch.
+    // After enabling the internal entropy source (SAR ADC), random numbers are generated based on the thermal noise in the system and the asynchronous clock mismatch.
     // For more detail please refer to https://docs.espressif.com/projects/esp-idf/en/v5.3/esp32c3/api-reference/system/random.html and the technical reference manual
     esp_fill_random(obuf, len);
 
