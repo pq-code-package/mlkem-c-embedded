f5859fe8c1c1d40f9e73f77d639e65a59b20fa97
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-08-16 14:28:27 +0800

    add comment for rng
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- hal/randombytes.c
+++ hal/randombytes.c
@@ -4,6 +4,9 @@
 #ifdef RISCV
 #include <esp_random.h>
 int randombytes(uint8_t *obuf, size_t len) {
+    // NOTE:
+    // After enableing the internal entropy source (SAR ADC), random numbers are then generated based on the thermal noise in the system and the asynchronous clock mismatch.
+    // For more detail please refer to https://docs.espressif.com/projects/esp-idf/en/v5.3/esp32c3/api-reference/system/random.html and the technical reference manual
     esp_fill_random(obuf, len);
 
     return 0;
